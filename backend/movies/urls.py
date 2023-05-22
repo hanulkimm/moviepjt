e6239f6ebf65edb2fe301204c2dbfc1077ea798a
hanulkimm <hanulkim2000@naver.com>
2023-05-22 12:26:23 +0900

    update api url

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movies/location/<int:location_pk>/', views.location_movies),
     path('movies/detail/<int:movie_pk>/', views.movie_detail),
-    path('movies/<int:movie_pk/comments/', views.comment_create),
+    path('movies/<str:state>/<str:city>/', views.location_movies),
+    path('movies/<int:movie_pk>/comments/', views.comment_create),
     path('comments/<int:comment_pk>/', views.comment_edit),
-
+    path('comments/<int:comment_pk>/likes/', views.comment_likes),
 ]
--- backend/my_backend/urls.py
+++ backend/my_backend/urls.py
@@ -22,4 +22,3 @@ urlpatterns = [
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls'))
 ]
-
