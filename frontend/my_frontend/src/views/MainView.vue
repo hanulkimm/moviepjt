aa239cef64026e2bf60ccb648157aafd65ce6105
hanulkimm <hanulkim2000@naver.com>
2023-05-24 00:19:32 +0900

    need correction of route

--- frontend/my_frontend/src/views/MainView.vue
+++ frontend/my_frontend/src/views/MainView.vue
@@ -2,43 +2,26 @@
   <div id="main">
     <nav class="navbar navbar-dark bg-dark sticky-top">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Logo</a>
-        <router-link @click.native="resetMovieList" to="/home">Home</router-link>
+        <img src="../assets/movie_icon.png" alt="">
+        <!-- <router-link to="/">Main</router-link> | -->
+        <router-link :to="{name:'signup'}">Signup</router-link> |
+        <router-link :to="{name:'login'}">Login</router-link> |
+        <p class="btn" @click="logout" >LogOut</p>
+        <router-link @click.native="resetMovieList" to="/home">CineMap</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Hello, {{user}}</h5>
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Profile</h5>
             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu dropdown-menu-dark">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="d-flex mt-3" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-success" type="submit">Search</button>
-            </form>
-          </div>
+            <div class="profile">
+              <img src="" class="profile-img" alt="Profile Image">
+              <h3 class="profile-username">{{this.$store.state.username}}</h3>
+            </div>
+         </div>
         </div>
       </div>
     </nav>
@@ -145,6 +128,23 @@ export default {
 
 
 <style>
+.profile {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.profile-img {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+}
+
+.profile-username {
+  font-size: 24px;
+  color: white;
+}
+
 nav {
   padding: 30px;
 }
